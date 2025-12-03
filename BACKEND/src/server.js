77b770d4cdc8da5a5a69ef6e9526f481bfb68be6
garvitmh <garvitmaheshwari.000@gmail.com>
2025-12-03 22:09:18 +0530

    everything is done

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -4,23 +4,36 @@ import { ConnectDB } from "./config/db.js";
 import dotenv from "dotenv"
 import cors from 'cors';
 import rateLimit from "./middleware/RateLimiter.js";
+import path from "path"
 
 const app = express();
 dotenv.config();
 
+const __dirname = path.resolve();
 
 // Add these middleware before routes
-app.use(cors());
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors());
+}
+
+
 app.use(express.json());
 app.use(rateLimit)
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/notes", notesRoutes);
 
-const PORT =  9000;
-ConnectDB().then(()=>{
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../FRONTEND/dist")))
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../FRONTEND", "dist", "index.html"))
+  })
+}
+
+const PORT = 9000;
+ConnectDB().then(() => {
   const server = app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
-});
+    console.log(`Server is running on port: ${PORT}`);
+  });
 
 })
 
